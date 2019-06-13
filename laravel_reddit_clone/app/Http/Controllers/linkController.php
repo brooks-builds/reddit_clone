9c4992f804f3af44425d191a3b1996f4d6150bdd
Brooks Patton <brooks@brookzerker.com>
2019-06-13 16:58:30 -0600

    I wasn't using that user object anyways

--- laravel_reddit_clone/app/Http/Controllers/linkController.php
+++ laravel_reddit_clone/app/Http/Controllers/linkController.php
@@ -11,7 +11,6 @@ class linkController extends Controller
 {
     function upvote($id)
     {
-        $user = Auth::user();
         $previousUrl = URL::previous();
 
         if(Auth::check())
@@ -24,7 +23,6 @@ class linkController extends Controller
 
     function downvote($id)
     {
-        $user = Auth::user();
         $previousUrl = URL::previous();
 
         if(Auth::check())
